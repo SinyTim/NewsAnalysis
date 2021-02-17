ab4f65f23f8d599f7bb4c6eea2485e20c7a80e28
SinyTim <sinytim@protonmail.com>
2021-02-18 00:12:49 +0300

    .

--- articles_aggregator/source/python/aggregator/data_platform/orchestrator/dag.py
+++ articles_aggregator/source/python/aggregator/data_platform/orchestrator/dag.py
@@ -1,4 +1,15 @@
 import dagster
+import dagster_pyspark
+from dagster.core.definitions.no_step_launcher import no_step_launcher
+
+
+mode_local = dagster.ModeDefinition(
+    name='local',
+    resource_defs={
+        'pyspark_step_launcher': no_step_launcher,
+        'pyspark': dagster_pyspark.pyspark_resource
+    }
+)
 
 
 @dagster.solid
@@ -6,12 +17,17 @@ def get_name(context, default: str) -> str:
     return default or 'dagster'
 
 
-@dagster.solid
+@dagster.solid(required_resource_keys={'pyspark', 'pyspark_step_launcher'})
 def hello(context, name: str) -> None:
+    spark = context.resources.pyspark.spark_session
     context.log.info(f'Hello, {name}!')
+    x = range(1000)
+    x = spark.sparkContext.parallelize(x)
+    context.log.info(str(x.take(5)))
+    return x
 
 
-@dagster.pipeline
+@dagster.pipeline(mode_defs=[mode_local])
 def hello_pipeline():
     name = get_name()
     hello(name)
@@ -43,3 +59,6 @@ if __name__ == "__main__":
     }
 
     dagster.execute_pipeline(hello_pipeline, run_config=run_config)
+
+# dagster-daemon run
+# dagit -f dag.py
