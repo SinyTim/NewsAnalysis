97021706ca810c0eb4f4a893d00175023711eb0f
SinyTim <sinytim@protonmail.com>
2021-03-03 12:35:49 +0300

    .

---
+++ articles_aggregator/source/python/aggregator/data_platform/orchestrator/run_job.py
@@ -0,0 +1,15 @@
+import sys
+
+from pyspark.sql import SparkSession
+
+import aggregator
+
+
+if __name__ == '__main__':
+    job_class_str = sys.argv[1]
+
+    spark = SparkSession.builder.getOrCreate()
+
+    params = {'spark': spark}
+
+    eval(job_class_str)(**params).run()
