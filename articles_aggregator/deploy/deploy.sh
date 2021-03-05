8d5a0399b02028435737dfaa497ad544cd27a698
SinyTim <sinytim@protonmail.com>
2021-03-05 03:15:10 +0300

    .

--- articles_aggregator/deploy/deploy.sh
+++ articles_aggregator/deploy/deploy.sh
@@ -1,6 +1,13 @@
 
-git clone https://github.com/SinyTim/NewsAnalysis.git
+# git clone https://github.com/SinyTim/NewsAnalysis.git
+
+cd $HOME/NewsAnalysis/articles_aggregator
 pip install -r requirements.txt
+
+cd $HOME/NewsAnalysis/articles_aggregator/packages
+source build.sh
+
 export DAGSTER_HOME=$HOME/NewsAnalysis/articles_aggregator/dagster_home
+cd $DAGSTER_HOME
 $HOME/.local/bin/dagster-daemon run &
 $HOME/.local/bin/dagit -h 0.0.0.0 -p 3000 &
