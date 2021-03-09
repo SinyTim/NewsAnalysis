461fcca0f89bdefe3495c6bbdec531ce21afeecd
SinyTim <sinytim@protonmail.com>
2021-03-09 22:14:36 +0300

    .

--- articles_aggregator/source/python/aggregator/data_platform/orchestrator/run.py
+++ articles_aggregator/source/python/aggregator/data_platform/orchestrator/run.py
@@ -6,14 +6,29 @@ from aggregator.data_platform.orchestrator import solids
 
 if __name__ == '__main__':
 
-    repository.preset_local.run_config.pop('solids')
+    # repository.preset_dev.run_config.pop('solids')
+    #
+    # dagster.execute_solid(
+    #     solids.solid_structured_tutby,
+    #     mode_def=repository.mode_local,
+    #     input_values={
+    #         'path_source': 'raw/html/tutby.delta',
+    #         'path_target': 'structured/tutby.delta',
+    #     },
+    #     run_config=repository.preset_dev.run_config
+    # )
 
-    dagster.execute_solid(
-        solids.solid_structured_komzdrav,
-        mode_def=repository.mode_local,
-        input_values={
-            'path_source': 'raw/html/komzdrav.delta',
-            'path_target': 'structured/komzdrav.delta',
-        },
-        run_config=repository.preset_local.run_config
+    x = [
+        'solid_curated_4gkb', 'solid_curated_komzdrav', 'solid_generator_4gkb',
+        'solid_generator_komzdrav', 'solid_scraper_4gkb', 'solid_scraper_komzdrav',
+        'solid_structured_4gkb', 'solid_structured_komzdrav'
+    ]
+
+    for c in x:
+        if c in repository.preset_dev.run_config['solids']:
+            repository.preset_dev.run_config['solids'].pop(c)
+
+    dagster.execute_pipeline(
+        repository.pipeline_main,
+        preset='dev',
     )
--- articles_aggregator/source/python/aggregator/temp/_temp.py
+++ articles_aggregator/source/python/aggregator/temp/_temp.py
@@ -1,15 +1,33 @@
 from pathlib import Path
 
 import pandas as pd
+from pyspark.sql import SparkSession
 
+from aggregator.data_platform.utils.function import read_delta
 from aggregator.data_platform.utils.function import read_parquet
 
 
 if __name__ == '__main__':
 
-    pd.set_option('display.max_columns', 10)
+    # pd.set_option('display.max_columns', 10)
+    #
+    # path = Path(r'C:\Users\Tim\Documents\GitHub\NewsAnalysis\data\_data\analytics\topicwords')
+    # df = read_parquet(path)
+    #
+    # print(df)
 
-    path = Path(r'C:\Users\Tim\Documents\GitHub\NewsAnalysis\data\_data\analytics\topicwords')
-    df = read_parquet(path)
+    spark = SparkSession.builder \
+        .config('spark.jars.packages', 'io.delta:delta-core_2.12:0.8.0') \
+        .config('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension') \
+        .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog') \
+        .getOrCreate()
 
-    print(df)
+    path0 = Path(r'C:\Users\Tim\Documents\GitHub\NewsAnalysis\articles_aggregator\data\_data\curated\article.delta')
+    path1 = Path(r'C:\Users\Tim\Documents\GitHub\NewsAnalysis\articles_aggregator\data\_data\analytics\preprocessed.delta')
+
+    df0 = read_delta(spark, path0)
+    df1 = read_delta(spark, path1)
+
+    df = df0.join(df1, on='url_id')
+
+    print(df.show(3, True))
